50d442d148c4f9c00da6d42d37e293d79014c354
Kennyrogers001 <skillhunt3r01@gmail.com>
2024-04-12 01:00:23 +0800

    minor scripting

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-
 const runner=document.getElementById("delivery-runner");
 const runnerDetails=document.getElementsByClassName("delivery-runner-details")[0];
 const self=document.getElementById("self-collect");
 const selfDetails=document.getElementsByClassName("delivery-self-details")[0];
+
+
+/*If #delivery-runner is checked:
+location-type and datetime become required.
+message becomes required if #custom is checked.
+If #self-collect is checked:
+datetime becomes required.*/
+function requiredRadio() {
+    var datetimeRunner = document.getElementById("datetime-runner");
+    var datetimeSelf = document.getElementById("datetime-self");
+    var customRadio = document.getElementById("custom");
+    var messageInput = document.getElementById("message");
+    var customLocation = document.getElementById("custom-location");
+
+    if (runner.checked) {
+        datetimeRunner.required = true;
+        datetimeSelf.required = false;
+        var locationType = document.querySelectorAll("input[name='location-type']");
+        locationType.forEach(function(input) {
+            input.required = true;
+        });
+    
+    } else if (self.checked) {
+        datetimeRunner.required = false;
+        datetimeSelf.required = true;
+        var locationType = document.querySelectorAll("input[name='location-type']");
+        locationType.forEach(function(input) {
+            input.required = false;
+        });
+    }
+}
 
 function deliveryDetails(){
     if(runner.checked){
@@ -13,18 +43,22 @@ function deliveryDetails(){
           runnerDetails.style.display = "none";
         }
         selfDetails.style.display = self.checked ? "block" : "none";
-        console.log("delivery on the way");
-    }
+    }   
     if (self.checked) {
         runnerDetails.style.display="none";
         selfDetails.style.display="block";
-        console.log("delivery on the way");
  
     } else {
         runnerDetails.style.display="none";
         selfDetails.style.display="none";
-        console.log("delivery on the way");
  
     }
-runner.addEventListener("click",deliveryDetails);
-self.addEventListener("click",deliveryDetails);
+
+runner.addEventListener("click",function() {
+    deliveryDetails();
+    requiredRadio();
+});
+self.addEventListener("click",function() {
+    deliveryDetails();
+    requiredRadio();
+});
